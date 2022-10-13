cb977bdd06eccdf76d221a66d3f70d954c9c8124
0xgareth <gareth@garethv.xyz>
2022-10-13 17:09:48 +0100

    init 09

--- threejs-journey/09-geometries/src/script.js
+++ threejs-journey/09-geometries/src/script.js
@@ -12,8 +12,20 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
+
+const positionsArray = new Float32Array([
+    // x, y, z
+    0,0,0,
+    0,1,0,
+    1,0,0
+])
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3)
+
+const geometry = new THREE.BufferGeometry()
+geometry.setAttribute('position', positionsAttribute)
+
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
